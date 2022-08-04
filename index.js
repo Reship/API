4a12dac83a2e68e5df4af7221efbbdc241301217
Reshi <reshipandit@gmail.com>
2022-08-04 14:07:28 +0530

    minor changes

--- index.js
+++ index.js
@@ -6,14 +6,29 @@ const putButton = document.querySelector("#putPost");
 const patchButton = document.querySelector("#patchPost");
 const deleteButton = document.querySelector("#deletePost");
 
-function getPosts(){
-  fetch(apiEndPoint)
-  .then(response => {
-   response.json()
-   .then((posts)=> console.log(posts));
-  });
-}
+const getPosts = async () =>{
+  const response = await fetch(apiEndPoint);
+  const posts = await response.json();
+  return posts;
+};
+getButton.addEventListener("click",async() => {
+  const posts = await getPosts();
 
-getButton.addEventListener("click",() => {
-  getPosts();
+  const table = `<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+    </tr>
+  </thead>
+  <tbody>
+  ${posts.map(post =>`<tr>
+  <th scope="row">${post.id}</th>
+  <td>${post.title}</td>
+  </tr>`)
+  .join("\n")}
+    
+  </tbody>
+  </table>`;
+  document.querySelector('#table').innerHTML = table;
 });
